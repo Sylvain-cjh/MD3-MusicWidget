674d5fa12e27afec44a85116b76b28a75b288e14
syyyc <syyyc@SY-C>
2026-06-29 22:15:47 +0800

    添加项目文件。

---
+++ MusicWidget/AnimationHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Control = System.Windows.Controls.Control;
+using TextBlock = System.Windows.Controls.TextBlock;
+
+using Color = System.Windows.Media.Color;
+using Brush = System.Windows.Media.Brush;
+using Colors = System.Windows.Media.Colors;
+
+namespace MusicWidget
+{
+    public static class AnimationHelper
+    {
+        public static void AnimateColorTo(this UIElement element, DependencyProperty property, Color targetColor, double durationMs = 350)
+        {
+            Color startColor = Colors.Transparent;
+            Brush? currentBrush = null;
+
+            if (property == Control.BackgroundProperty) currentBrush = (Brush)element.GetValue(Control.BackgroundProperty);
+            else if (property == Control.ForegroundProperty) currentBrush = (Brush)element.GetValue(Control.ForegroundProperty);
+            else if (property == TextBlock.ForegroundProperty) currentBrush = (Brush)element.GetValue(TextBlock.ForegroundProperty);
+
+            if (currentBrush is SolidColorBrush scb) startColor = scb.Color;
+            else startColor = targetColor;
+
+            SolidColorBrush animatedBrush = new SolidColorBrush(startColor);
+            element.SetValue(property, animatedBrush);
+
+            ColorAnimation animation = new ColorAnimation
+            {
+                To = targetColor,
+                Duration = TimeSpan.FromMilliseconds(durationMs),
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
+            };
+
+            animatedBrush.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+    }
+}
---
+++ MusicWidget/SettingsWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Application = System.Windows.Application;
+
+namespace MusicWidget
+{
+    public partial class SettingsWindow : Window
+    {
+        private bool _isLoaded = false;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            SyncFromConfig();
+
+            this.Topmost = AppConfig.Current.IsTopmost;
+            ApplyCurrentTheme();
+        }
+
+        public void SyncFromConfig()
+        {
+            _isLoaded = false; 
+
+            ChkTopmost.IsChecked = AppConfig.Current.IsTopmost;
+            ChkClickThrough.IsChecked = AppConfig.Current.IsClickThrough;
+            ChkLightMode.IsChecked = AppConfig.Current.IsLightMode;
+            ChkBgFade.IsChecked = AppConfig.Current.EnableBgFade;
+
+            _isLoaded = true;
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed) this.DragMove();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e) => this.Close();
+
+        public void ApplyCurrentTheme()
+        {
+            if (Application.Current.MainWindow is MainWindow mainWin)
+            {
+                var palette = mainWin.CurrentPalette;
+                RootBorder.Background = new SolidColorBrush(palette.Background);
+                HeaderTxt.Foreground = new SolidColorBrush(palette.TextPrimary);
+                CloseIcon.Foreground = new SolidColorBrush(palette.TextPrimary);
+                SettingsCard.Background = new SolidColorBrush(palette.SecondaryContainer);
+                SettingsCard.Foreground = new SolidColorBrush(palette.TextPrimary);
+            }
+        }
+
+        private void Topmost_Changed(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded) return;
+            AppConfig.Current.IsTopmost = ChkTopmost.IsChecked == true;
+            AppConfig.Save();
+            this.Topmost = AppConfig.Current.IsTopmost;
+            if (Application.Current.MainWindow is MainWindow mainWin) mainWin.ApplyConfig();
+        }
+
+        private void ClickThrough_Changed(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded) return;
+            AppConfig.Current.IsClickThrough = ChkClickThrough.IsChecked == true;
+            AppConfig.Save();
+            if (Application.Current.MainWindow is MainWindow mainWin) mainWin.ApplyConfig();
+        }
+
+        private void LightMode_Changed(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded) return;
+            AppConfig.Current.IsLightMode = ChkLightMode.IsChecked == true;
+            AppConfig.Save();
+            AppConfig.UpdateGlobalThemeColor();
+            if (Application.Current.MainWindow is MainWindow mainWin) mainWin.ForceRefreshColors();
+        }
+
+        private void BgFade_Changed(object sender, RoutedEventArgs e)
+        {
+            if (!_isLoaded) return;
+            AppConfig.Current.EnableBgFade = ChkBgFade.IsChecked == true;
+            AppConfig.Save();
+            if (Application.Current.MainWindow is MainWindow mainWin) mainWin.ToggleBgFade();
+        }
+    }
+}
